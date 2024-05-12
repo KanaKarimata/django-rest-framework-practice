0a35c0412cfc7837cdfb6fc4c2129c36d106f0b0
KanaKarimata <kana.lt22@gmail.com>
2024-05-12 13:17:50 +0900

    add serializers for views

---
+++ product/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+# from .views import LatestProductsList
+from product import views
+
+urlpatterns = [
+    path('latest-products/', views.LatestProductsList.as_view()),
+]
